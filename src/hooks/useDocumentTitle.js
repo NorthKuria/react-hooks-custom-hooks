cdc7ca9e351b859442d9a7763934ff4008396376
northkuria <kuri4martin@gmail.com>
2022-07-27 13:12:44 +0300

    Implement custom hooks

--- src/hooks/useDocumentTitle.js
+++ src/hooks/useDocumentTitle.js
@@ -1,9 +1,35 @@
-import { useEffect } from "react";
+import React, { useEffect, useState } from 'react'
+import About from './About'
+import ArticleList from './ArticleList'
+import useQuery from '../hooks/useQuery'
 
-function useDocumentTitle(pageTitle) {
+function HomePage() {
+  // fetch data for posts
+  const { data: posts, isLoaded } = useQuery('http://localhost:4000/posts')
+  // const [isLoaded, setIsLoaded] = useState(false);
+  // const [posts, setPosts] = useState([]);
+
+  // useEffect(() => {
+  //   setIsLoaded(false);
+  //   fetch("http://localhost:4000/posts")
+  //     .then((r) => r.json())
+  //     .then((posts) => {
+  //       setPosts(posts);
+  //       setIsLoaded(true);
+  //     });
+  // }, []);
+
+  // set the document title
   useEffect(() => {
-    document.title = pageTitle;
-  }, [pageTitle]);
+    document.title = 'Underreacted | Home'
+  }, [])
+
+  return (
+    <>
+      <About />
+      {isLoaded ? <ArticleList posts={posts} /> : <h3>Loading...</h3>}
+    </>
+  )
 }
 
-export default useDocumentTitle;
+export default HomePage
